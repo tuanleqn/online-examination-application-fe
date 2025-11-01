45ad4895e30f74f791ddb879d1adc997acca4d8b
Le Duc Anh Tuan <tuanleqn04@gmail.com>
2025-11-01 10:30:55 +0700

    feat: add instructor dashboard

---
+++ src/routes/MainRoutes.tsx
@@ -0,0 +1,39 @@
+import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import InstructorDashboard from '@/pages/InstructorPage/InstructorDashboard'
+
+export default function MainRoutes() {
+  return (
+    <>
+      <ToastContainer
+        position='top-right'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
+
+      <Routes>
+        {/* <Route path='/' element={<HomePage />}>
+          {' '}
+        </Route> */}
+        <Route path='/instructor' element={<InstructorDashboard />}>
+          {/* <Route path='create-test' element={<CreateTest />} /> */}
+          {/* <Route path='test/:testId/questions' element={<ManageQuestions />} /> */}
+        </Route>
+        {/* <Route path='/student' element={<StudentDashboard />}>
+          <Route path='test/:testId' element={<StudentTest />} />
+        </Route> */}
+
+        {/* <Route path='/auth' element={<AuthPage />}>
+          {' '}
+        </Route> */}
+      </Routes>
+    </>
+  )
+}
