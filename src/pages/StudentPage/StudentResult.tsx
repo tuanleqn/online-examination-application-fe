4105a9047c1e9675cbf9c2ed08d63dcfe4206540
Le Duc Anh Tuan <tuanleqn04@gmail.com>
2025-11-08 11:56:32 +0700

    fix: update default testId to 2 in StudentResult component

--- src/pages/StudentPage/StudentResult.tsx
+++ src/pages/StudentPage/StudentResult.tsx
@@ -37,7 +37,7 @@ const StudentResult = () => {
 
       // Temporary: Sử dụng giá trị mặc định
       const studentUserId = 3
-      const testIdNumber = 1 // Mặc định testId = 1, sau này sẽ lấy từ params
+      const testIdNumber = 2 // Mặc định testId = 1, sau này sẽ lấy từ params
 
       const data = await testsApi.getStudentSubmission({
         testId: testIdNumber,
