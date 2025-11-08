dd71782a5314ea4fd9bb2a1ff05dd45772cab61c
millerbright <sang.nguyennqs@hcmut.edu.vn>
2025-11-08 10:02:48 +0700

    update student

--- src/apis/tests.api.ts
+++ src/apis/tests.api.ts
@@ -1,12 +1,13 @@
+import type { Test } from '@/models/Test/Test'
 import type { TestResultResponse, StudentSubmissionResponse } from '@/models/Test/TestResult'
 import { fetcher } from './fetcher'
 import type { AxiosResponse } from 'axios'
 
+
 export const testsApi = {
-  // Get all tests created by the instructor
-  getAllTests: async () => {
+  getAllTests: async (): Promise<Test[]> => {
     try {
-      const response = await fetcher.get('/test/tests')
+      const response: AxiosResponse<Test[]> = await fetcher.get('/query?function=TOP_GAINERS_LOSERS&apikey=demo')
       return response.data
     } catch (error) {
       console.error('Error fetching tests:', error)
@@ -48,6 +49,62 @@ export const testsApi = {
       return response.data
     } catch (error) {
       console.error('Error fetching student submission:', error)
+      throw error
+    }
+  },
+
+  //Get test of student by StudentId
+  getTestsByStudentId: async (studentId: number): Promise<any> => {
+    try {
+      const response: AxiosResponse<any> = await fetcher.get(`/history/student/${studentId}`, {
+        params: { studentId }
+      })
+      return response.data
+    } catch (error) {
+      console.error('Error fetching tests for student:', error)
+      throw error
+    }
+  },
+
+  //Auto_save test draft
+  SaveTestProgress: async (): Promise<any> => {
+    try {
+      const response: AxiosResponse<any> = await fetcher.post(`/progress/submission/`)
+      return response.data
+    } catch (error) {
+      console.error('Error saving test progress:', error)
+      throw error
+    }
+  },
+
+  // Get current test draft
+  GetTestProgress: async (Query: { StudentId: number, testId: number }): Promise<any> => {
+  try {
+    const response: AxiosResponse<any> = await fetcher.get(
+      `/progress/current-progress`, 
+      {
+        params: {
+          studentUserId: Query.StudentId, 
+          testId: Query.testId
+        }
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching test progress:', error);
+    throw error;
+    }
+  },
+
+  //Verify passcode for test
+  VerifyTestPasscode: async (passcode: string): Promise<any> => {
+    try {
+      const response: AxiosResponse<any> = await fetcher.get(`/test/verify_passcode/${passcode}`, {
+params: { passcode }
+      })
+      return response.data
+    } catch (error) {
+      console.error('Error verifying test passcode:', error)
       throw error
     }
   }
